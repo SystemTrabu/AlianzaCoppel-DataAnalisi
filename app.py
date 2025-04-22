c0dd286c2bfb66eb0ef2d39d4e7403dca9c0a196
Arturo Torres <128834206+SystemTrabu@users.noreply.github.com>
2025-04-22 14:42:51 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from App import create_app
 from dotenv import load_dotenv
 load_dotenv()
@@ -5,5 +6,6 @@ load_dotenv()
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))  # Railway define PORT=8080
+
     app.run(debug=True)
