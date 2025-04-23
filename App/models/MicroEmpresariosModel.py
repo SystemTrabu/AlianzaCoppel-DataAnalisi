9dce2cae4dfff5ea4010dad3d28124613c799391
Manuel Balan Euan <127346247+ManuBalan03@users.noreply.github.com>
2025-04-23 12:26:53 -0600

    modelo actuzalizado

--- App/models/MicroEmpresariosModel.py
+++ App/models/MicroEmpresariosModel.py
@@ -14,6 +14,8 @@ class MicroEmpresario(db.Model):
     fecha_registro = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
     fecha_nacimiento = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
     nivel_educativo= db.Column(db.String(100), nullable=False)
+    actividad = db.Column(db.String(255), nullable=True)  # permite NULL por defecto
+    estado = db.Column(db.String(100), nullable=True)  
     colaborador_id = db.Column(db.Integer, db.ForeignKey('colaborador.id'), nullable=False)
     empresa_id = db.Column(db.Integer, db.ForeignKey('empresas.id'), nullable=False)
     cursos_terminados = db.relationship('CursosTerminados', backref='microempresario', lazy=True)
